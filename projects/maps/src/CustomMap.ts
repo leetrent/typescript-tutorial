75c7291419fc70cba096d0d70ce77ae42b3c03a2
LeeTrent <lee.trent.1@gmail.com>
2020-09-23 08:20:08 -0500

    Adds user marker

--- projects/maps/src/CustomMap.ts
+++ projects/maps/src/CustomMap.ts
@@ -1,9 +1,29 @@
+import { User } from './User';
+import { Company } from './Company';
+
 export class CustomMap {
+  
   private googleMap: google.maps.Map;
+  
   constructor(divID: string) {
     this.googleMap = new google.maps.Map(document.getElementById(divID), {
       zoom: 1,
       center: { lat: 0, lng: 0 }
     });
   }
+
+  addUserMarker(user: User): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: user.location.lat,
+        lng: user.location.lng
+      }
+    });
+  }
+
+  addCompanyMarker(company: Company): void {
+
+  }
+
 }
--- projects/maps/src/index.ts
+++ projects/maps/src/index.ts
@@ -1,11 +1,14 @@
-// import { User } from './User';
+import { User } from './User';
 // import { Company } from './Company';
 import { CustomMap } from './CustomMap';
 
-// const user = new User();
-// console.log(user);
+
 
 // const company = new Company();
 // console.log(company);
 
-new CustomMap('map');
+const user = new User();
+console.log(user);
+
+const customMap = new CustomMap('map');
+customMap.addUserMarker(user);
